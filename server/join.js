03f51b7e985f526a2456d03d17cd9801d8a3625b
marcelojoc <marcelojoc@gmail.com>
2021-12-20 10:11:29 -0300

    join Split y uppercase

---
+++ server/join.js
@@ -0,0 +1,19 @@
+
+
+const elements = ["Fire", "Air", "Water"];
+
+let rtaFinal = '';
+const separator = '--';
+for (let index = 0; index < elements.length; index++) {
+    const element = elements[index];
+    rtaFinal = rtaFinal + element + separator;
+}
+
+const rta = elements.join('--')
+console.log('for', rtaFinal);
+console.log('join', rta);
+
+const title = 'Curso de manipulación de arrays';
+
+const urlFinal = title.split(' ').join('-').toLowerCase();  // usando split  para  separar  segun un patron en un array --usaondo join para  juntarlo nuevamente a un String  usando un patron de union
+console.log(urlFinal);
