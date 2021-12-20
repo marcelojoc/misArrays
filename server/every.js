0db3765a22cfed462e0f3d8711c2c882c115083e
Marcelo Contreras <marcelojoc@gmail.com>
2021-12-20 13:41:31 -0300

    update every js
    comentario  sobre every

--- server/every.js
+++ server/every.js
@@ -1,6 +1,13 @@
 const numbers = [1,30,39,29,10,13];
 
+/**
+El método every() de JavaScript funciona en arreglos y lo que hace es verificar si todos los elementos de un arreglo pasan un test. El test es una función.
 
+El método every() comienza a ejecutar la función de test para cada elemento del arreglo y va a retornar false o true, según sea el caso:
+
+Falso: el método retornara false si es que la función de test retorne false para algún elemento y luego de eso dejará de ejecutar la función test en los siguientes elementos.
+Verdadero: si la función de test no retornar ningún valor false, en ese caso el método every() devolverá true. Hay que tener en cuenta que este método no cambia el arreglo original.
+**/
 let rta = true;
 for (let index = 0; index < numbers.length; index++) {
     const element = numbers[index];
